23c5823dd912275e27e8f869740d395deb8dbf39
LukaPrcic12345laptop <bobsagott17@gmail.com>
2023-03-18 10:32:47 +0100

    User migration done

--- SuKraftBekend/database/migrations/2014_10_12_000000_create_users_table.php
+++ SuKraftBekend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('lat_name');
+            $table->string('username');
+            $table->string('phoneNo');
+            $table->enum('role',['seller','admin','user',]);
+            $table->boolean('verified');
+            $table->foreignId('mediaID')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
